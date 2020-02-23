d582833d327f7d2a0a2aa5a17704a4d88ecc671e
tx-10 <tlee0027@student.monash.edu>
2020-02-23 17:16:10 +0800

    ui

--- strangeplanetgame-master/src/App.js
+++ strangeplanetgame-master/src/App.js
@@ -1,192 +1,279 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 import { AwesomeButton } from "react-awesome-button";
-import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
-import './App.css';
-import $ from 'jquery';
-import  {DisplayModal}  from  './DisplayModal.js'
-import logo from './logo.png'
+import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss"; //need this
+import "./App.css";
+import $ from "jquery";
+import { DisplayModal } from "./DisplayModal.js";
+import logo from "./logo.png";
 
-var style = {
-  backgroundColor: "#E700E7",
-  borderTop: "2px solid #E7E7E7",
-  textAlign: "center",
-  // padding: "20px",
-  topMargin: "100px",
-  position: "fixed",
-  left: "0",
-  bottom: "0",
-  height: "50px",
-  // height: "100%",
-  fontSize: "7px",
-  width: "100%"
-}
+// var style = {
+//   backgroundColor: "#E700E7",
+//   borderTop: "2px solid #E7E7E7",
+//   textAlign: "center",
+//   // padding: "20px",
+//   topMargin: "100px",
+//   position: "fixed",
+//   left: "0",
+//   bottom: "0",
+//   height: "50px",
+//   // height: "100%",
+//   fontSize: "7px",
+//   width: "100%"
+// }
 
 var phantom = {
-display: 'block',
-padding: '10px',
-height: '20px',
-width: '100%',
-}
- 
+  display: "block",
+  padding: "10px",
+  height: "20px",
+  width: "100%"
+};
+
 export default class App extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			modalVisibility : "modal",
-			result : '',
-			currentCaption : "",
-			counter : 0,
-			imageList : JSON.parse(window.sessionStorage.getItem('data')),
-			start : false
-		};
-		this.toggleModal = this.toggleModal.bind(this);
-		this.closeFunction = this.closeFunction.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisibility: "modal",
+      result: "",
+      currentCaption: "",
+      counter: 0,
+      imageList: JSON.parse(window.sessionStorage.getItem("data")),
+      start: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.closeFunction = this.closeFunction.bind(this);
+		this.inputRef = React.createRef();
+		this.modalRef = React.createRef();
+  }
 
-	componentDidMount() {
+  componentDidMount() {
     var that = this;
-		var gcp = "http://35.184.2.171:8080/";
-		var local = "http://localhost:8080/";
-		var firebase_hosted = "https://us-central1-instagram-comic-game.cloudfunctions.net/app"
-		$(document).ready(function(){
-				$.getJSON(firebase_hosted, function(result){
-            window.sessionStorage.setItem('data', JSON.stringify(result));
-            that.setState({imageList: JSON.parse(window.sessionStorage.getItem('data'))});
-          });
-				// $("#imageBox").attr("src", this.state.imageList[this.state.imageList.length-1].media_url);
-				// let currentCaption = this.state.imageList[this.state.imageList.length-1].caption;
-				// this.setState({currentCaption : currentCaption});
-			}
-    ) 
+    var gcp = "http://35.184.2.171:8080/";
+    var local = "http://localhost:8080/";
+    var firebase_hosted =
+      "https://us-central1-instagram-comic-game.cloudfunctions.net/app";
+    $(document).ready(function() {
+      $.getJSON(firebase_hosted, function(result) {
+        window.sessionStorage.setItem("data", JSON.stringify(result));
+        that.setState({
+          imageList: JSON.parse(window.sessionStorage.getItem("data"))
+        });
+      });
+      // $("#imageBox").attr("src", this.state.imageList[this.state.imageList.length-1].media_url);
+      // let currentCaption = this.state.imageList[this.state.imageList.length-1].caption;
+      // this.setState({currentCaption : currentCaption});
+    });
+  }
+
+  toggleModal() {
+    let visibility = "show-modal"
+    this.setState({ modalVisibility: visibility });
+    console.log("visibility " + this.state.modalVisibility);
+  }
+
+  closeFunction() {
+    this.setState({ modalVisibility: "modal" });
 	}
 
-	toggleModal(){
-		let visibility = this.state.modalVisibility === "show-modal" //"modal" ? "show-modal" : "show-modal"
-		this.setState({modalVisibility : visibility});
-		console.log(this.state.modalVisibility)
+	setModalRef = (node) => {
+		this.modalRef = node;
 	}
+	
+	handleClickOutside = event => {
+    const domNode = ReactDOM.findDOMNode(this.modalRef);
 
-	closeFunction() {
-		this.setState({modalVisibility : "modal"})
-	}
+    if (!domNode || !domNode.contains(event.target)) {
+      this.closeFunction();
+		}
+		
+		document.removeEventListener("click", this.handleClickOutside);
+		document.removeEventListener("ontouchend", this.handleClickOutside);
 
-	render() {
-		return (
-			<div className="fill-window">
-				<div className="centerImgContainer">
-					<img id="imageBox" src={logo} className='centerImage comic'></img> 
-					<div className="btnContainer">
-						<div className="row row-1">
-							<AwesomeButton className='btn randomButton leftmost'
-							type="primary"
-							size="medium"
-							ripple
-							onPress={() => {
-                console.log(this.state.imageList)
-								if (this.state.imageList !== null) {
-									this.state.counter =  Math.floor(Math.random() * this.state.imageList.length)
+  };
 
-									$("#imageBox").attr("src", this.state.imageList[this.state.counter].media_url);
-									let currentCaption = this.state.imageList[this.state.counter].caption;
-									console.log(currentCaption);
-									this.setState({currentCaption : currentCaption});
-								}
-							}}
-							>
-								Random
-							</AwesomeButton>
-							<AwesomeButton className='btn iotdButton'
-							size="medium"
-							type="secondary"
-							ripple
-							// onPress={() => handleIOTDButton()}
-							onPress={() => {
-								// let imageList = JSON.parse(window.sessionStorage.getItem('data'));
-								if (this.state.imageList !== null) {
-									$("#imageBox").attr("src", this.state.imageList[this.state.imageList.length-1].media_url);
-									let currentCaption = this.state.imageList[this.state.imageList.length-1].caption;
-									this.setState({currentCaption : currentCaption});
-									console.log(currentCaption);
-									// create a random this.state.counter
-								}
-							}}
-							>
-								#IOTD
-							</AwesomeButton>
-							<AwesomeButton className='btn helpButton'
-							size="small"
-							type="primary"
-							ripple
-							onPress={() => {
-								let result = "Welcome to the Strange Planet Guessing Game! \nClick on Random or Image of the Day (#IOTD) to get started.\
-															The caption is ONE of the words from the comic shown.\n Please enter your answer in the textbox without any spaces."
-								this.toggleModal()
-								// setTimeout(()=>this.toggleModal(), 4000);
+  render() {
+		const modalVisible = this.state.modalVisibility === "modal" ? false : true;
+    return (
+      <div className="fill-window">
+        <div className="centerImgContainer">
+          <img id="imageBox" src={logo} className="centerImage comic"></img>
+          <div className="btnContainer">
+            <div className="row row-1">
+              <AwesomeButton
+                className="btn randomButton leftmost"
+                type="primary"
+                size="medium"
+								ripple
+								disabled={modalVisible}
+                onPress={() => {
+                  console.log(this.state.imageList);
+                  if (this.state.imageList !== null) {
+                    this.state.counter = Math.floor(
+                      Math.random() * this.state.imageList.length
+                    );
 
-								this.state.result = result;
+                    $("#imageBox").attr(
+                      "src",
+                      this.state.imageList[this.state.counter].media_url
+                    );
+                    let currentCaption = this.state.imageList[
+                      this.state.counter
+                    ].caption;
+                    console.log(currentCaption);
+                    this.setState({ currentCaption: currentCaption });
+                  }
+                }}
+              >
+                Random
+              </AwesomeButton>
+              <AwesomeButton
+                className={`btn iotdButton`}
+                size="medium"
+                type="secondary"
+								ripple
+								disabled={modalVisible}
+                // onPress={() => handleIOTDButton()}
+                onPress={() => {
+                  // let imageList = JSON.parse(window.sessionStorage.getItem('data'));
+                  if (this.state.imageList !== null) {
+                    $("#imageBox").attr(
+                      "src",
+                      this.state.imageList[this.state.imageList.length - 1]
+                        .media_url
+                    );
+                    let currentCaption = this.state.imageList[
+                      this.state.imageList.length - 1
+                    ].caption;
+                    this.setState({ currentCaption: currentCaption });
+                    console.log(currentCaption);
+                    // create a random this.state.counter
+                  }
+                }}
+              >
+                #IOTD
+              </AwesomeButton>
+              <AwesomeButton
+                className={`btn helpButton`}
+                size="small"
+                type="primary"
+								ripple
+								disabled={modalVisible}
+                onPress={() => {
+                  let result =
+                    "Welcome to the Strange Planet Guessing Game! \nClick on Random or Image of the Day (#IOTD) to get started.\
+															The caption is ONE of the words from the comic shown.\n Please enter your answer in the textbox without any spaces.";
+                  this.toggleModal();
+                  // setTimeout(()=>this.toggleModal(), 4000);
 
-							}}//handleHelpButton()}
-							>
-								Help
-							</AwesomeButton>
+									this.state.result = result;
+									
+									//listen to click event outside of modal
+									setTimeout(() => {
+										document.addEventListener("click", this.handleClickOutside);
+										document.addEventListener("ontouchend", this.handleClickOutside);
+									}, 500)
+                }} //handleHelpButton()}
+              >
+                Help
+              </AwesomeButton>
             </div>
-					</div>
-					<div className="row row-2">
-						<input id="answer" className="textBox leftmost" type="text" name="Answer" placeholder="Answer"></input>
-						<AwesomeButton className='btn submitButton'
-						type="secondary"
-						size="small"
-						ripple
-						onPress={() => {
-							this.toggleModal()
+          </div>
+          <div className="row row-2">
+            <input
+              id="answer"
+              className="textBox leftmost"
+              type="text"
+              name="Answer"
+							placeholder="Answer"
+							disabled={modalVisible}
+              ref={el => {
+                this.inputRef = el;
+              }}
+            ></input>
+            <AwesomeButton
+              className={`btn submitButton`}
+              type="secondary"
+              size="small"
+							ripple
+							disabled={modalVisible}
+              onPress={() => {
+                this.toggleModal();
 
-							setTimeout(()=>this.toggleModal(), 1000);
-
-							let userAnswer = document.getElementById('answer').value;
-							if ( userAnswer !== null) {
-								userAnswer = userAnswer.toString().toLowerCase();
-								console.log("Your Answer: " + userAnswer);         
-								console.log("Actual Answer: "  + this.state.currentCaption)
-								if (userAnswer === this.state.currentCaption) {
-									console.log("yes");
-									let correctArr = ["c o r r e c t", "w o w", "g o o d  j o b", "p e r f e c t", "f l a w l e s s", "j o y  i n t e n s i f i e s", "s t r o n k s",
-									"You are a m a z i n g"]
-									let result = correctArr[Math.floor(Math.random() * correctArr.length)]
-									this.state.result = result;
+                let userAnswer = document.getElementById("answer").value;
+                if (userAnswer !== null) {
+                  userAnswer = userAnswer.toString().toLowerCase();
+                  console.log("Your Answer: " + userAnswer);
+                  console.log("Actual Answer: " + this.state.currentCaption);
+                  if (userAnswer === this.state.currentCaption) {
+                    console.log("yes");
+                    let correctArr = [
+                      "c o r r e c t",
+                      "w o w",
+                      "g o o d  j o b",
+                      "p e r f e c t",
+                      "f l a w l e s s",
+                      "j o y  i n t e n s i f i e s",
+                      "s t r o n k s",
+                      "You are a m a z i n g"
+                    ];
+                    let result =
+                      correctArr[Math.floor(Math.random() * correctArr.length)];
+                    this.state.result = result;
+                  } else {
+                    console.log("Wrong answer mate");
+                    let correctArr = [
+                      "r e g r e t",
+                      "d i s a p p o i n t m e n t",
+                      "You get z e r o points",
+                      "d e c e p t i o n",
+                      "e w w",
+                      "t r y  a g a i n"
+                    ];
+                    let result =
+                      correctArr[Math.floor(Math.random() * correctArr.length)];
+                    this.state.result = result;
+                  }
 								}
-								else {
-									console.log("Wrong answer mate");
-									let correctArr = ["r e g r e t", "d i s a p p o i n t m e n t", "You get z e r o points", "d e c e p t i o n", "e w w", "t r y  a g a i n"]
-									let result = correctArr[Math.floor(Math.random() * correctArr.length)]
-									this.state.result = result;
+								//listen to click event outside of modal
+								setTimeout(() => {
+									document.addEventListener("click", this.handleClickOutside);
+									document.addEventListener("ontouchend", this.handleClickOutside);
+								}, 500)
 
-								}
-							}
-						}}
-						>
-							Submit
-						</AwesomeButton>
-					</div>
-				</div>
 
-				<div className="row row-2">
-					<DisplayModal hidden={this.state.modalVisibility} text={this.state.result} closeFunction={this.closeFunction} />
-				</div>
+              }}
+            >
+              Submit
+            </AwesomeButton>
+          </div>
+        </div>
+
+        <div className="row row-2">
+          <DisplayModal
+            hidden={this.state.modalVisibility}
+            text={this.state.result}
+						closeFunction={this.closeFunction}
+						refer={this.setModalRef}
+          />
+        </div>
 
         <div className="container">
           <div style={phantom} />
-            <div style={style}>
-              <p>
-                <b>Disclaimer:</b> <br />
-                All works seen on the website belongs to the brilliant <a href="https://www.instagram.com/nathanwpylestrangeplanet/?hl=en">Nathan W. Pyle</a>. <br />
-                Minor edits have been made to certain images used so everything looks nice. <br />
-                <b>"Imagine Pleasant Nonsense"</b>
-              </p>
-            </div>
-			  </div>
-			</div>
-		);  
-	}
+          <div className="disclaimer">
+            <p>
+              <b>Disclaimer:</b> <br />
+              All works seen on the website belongs to the brilliant{" "}
+              <a href="https://www.instagram.com/nathanwpylestrangeplanet/?hl=en">
+                Nathan W. Pyle
+              </a>
+              . <br />
+              Minor edits have been made to certain images used so everything
+              looks nice. <br />
+              <b>"Imagine Pleasant Nonsense"</b>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
--- strangeplanetgame-master/src/DisplayModal.js
+++ strangeplanetgame-master/src/DisplayModal.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import "./Modal.css"
-import logo from './logo.png'
-import $ from 'jquery';
+import React, { Component } from "react";
+import "./Modal.css";
+import logo from "./logo.png";
+import $ from "jquery";
 
+export class DisplayModal extends Component {
+  constructor(props) {
+    super(props);
+  }
 
+//   componentDidMount = () => {
+//     document.addEventListener("click", this.handleClickOutside, true);
+//   };
 
-export class DisplayModal extends React.Component {
+//   componentWillUnmount = () => {
+//     document.removeEventListener("click", this.handleClickOutside, true);
+//   };
 
-    constructor(props){
-        super(props);
-        this.state = {closeFunction : () => {
-            this.setState({hidden : 'modal'})
-        }
-        ,
-        hidden : this.props.hidden
-    };
+//   handleClickOutside = event => {
+//     const domNode = ReactDOM.findDOMNode(this.wrapperRef);
 
-    }
-    
-    render(){
-        return (
-        <div id="main" className= {this.props.hidden} >
-            <div className="modal-content">
-                <span className="close-button" onClick={this.props.closeFunction}>&times; </span>
-                <h2>{this.props.text}</h2>
-                
-            </div>
+//     if (this.props.hidden === "show-modal" && !domNode.contains(event.target)) {
+//         console.log(this.props.hidden)
+//       //this.props.closeFunction();
+//     }
+//   };
+
+  render() {
+    return (
+      <div
+        id="main"
+        className={this.props.hidden}
+        ref={this.props.refer}
+      >
+        <div className="modal-content">
+          <span
+            className="close-button"
+            onClick={() => this.props.closeFunction()}
+          >
+            &times;{" "}
+          </span>
+          <h2>{this.props.text}</h2>
         </div>
-        )
-    }
-
+      </div>
+    );
+  }
 }
