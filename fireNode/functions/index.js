958cbb174a57348ab285fed345fe2db13a47a1bc
imuh0001 <imuh0001@student.monash.edu>
2020-02-26 00:36:28 +1100

    minor updates

--- fireNode/functions/index.js
+++ fireNode/functions/index.js
@@ -8,7 +8,7 @@ const functions = require('firebase-functions');
 // });
 
 express = require('express');
-AWS = require('aws-sdk');
+// AWS = require('aws-sdk');
 app = express();
 
 app.use((req, res, next) => {
@@ -16,59 +16,59 @@ app.use((req, res, next) => {
   next();
 });
 
-AWS.config.update({
-  region: 'ap-southeast-1',
-  accessKeyId: 'AKIAJV4YVFG3QP7BEP3Q',
-  secretAccessKey: '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'
-});
+// AWS.config.update({
+//   region: 'ap-southeast-1',
+//   accessKeyId: 'AKIAJV4YVFG3QP7BEP3Q',
+//   secretAccessKey: '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'
+// });
 
-docClient = new AWS.DynamoDB.DocumentClient();
-params = {
-  TableName: 'instagram_posts_v2',
-  FilterExpression: 'is_comic = :is_comic',
-  ExpressionAttributeValues: {':is_comic' : true}
-}
-var info = [];
+// docClient = new AWS.DynamoDB.DocumentClient();
+// params = {
+//   TableName: 'instagram_posts_v2',
+//   FilterExpression: 'is_comic = :is_comic',
+//   ExpressionAttributeValues: {':is_comic' : true}
+// }
+// var info = [];
 
-scanAWS();
+// scanAWS();
 
 app.get('/', (req, res) => {
   console.log("Requested");
   res.json(info);
 })
 
-class GuessPicture {
-  constructor() {
-    this.id;
-    this.media_url = "";
-    this.caption = "";
-  }
-}
+// class GuessPicture {
+//   constructor() {
+//     this.id;
+//     this.media_url = "";
+//     this.caption = "";
+//   }
+// }
 
-function scanAWS() {
-  console.log("Scanned again")  
-  docClient.scan(params, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      data.Items.forEach(item => {
-        let post = new GuessPicture();
-        post.id = item.id;
-        post.media_url = item.media_url;
-        post.caption = item.caption;
-        info.push(post);
-      })
+// function scanAWS() {
+  // console.log("Scanned again")  
+  // docClient.scan(params, (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //   else {
+  //     data.Items.forEach(item => {
+  //       let post = new GuessPicture();
+  //       post.id = item.id;
+  //       post.media_url = item.media_url;
+  //       post.caption = item.caption;
+  //       info.push(post);
+  //     })
 
-      info.sort(function(a, b) {
-        return a.id - b.id;
-      });
-      console.log("Scanned at : ");
-      var datetime = new Date();
-      console.log(datetime);
-      console.log("\n");
-    }
-  });
+  //     info.sort(function(a, b) {
+  //       return a.id - b.id;
+  //     });
+  //     console.log("Scanned at : ");
+  //     var datetime = new Date();
+  //     console.log(datetime);
+  //     console.log("\n");
+  //   }
+  // });
 }
 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
